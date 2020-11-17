314d32cf53afbdab2c8c58614e64e4cbed72cd4e
Kristina <christinabkv@gmail.com>
2020-11-17 21:19:42 +0300

    правки

--- build/js/main 2.js
+++ build/js/main 2.js
@@ -42,7 +42,7 @@
   let btnLeft = document.querySelector(".slider__button--left");
   let slides = document.querySelectorAll(".reviews__list blockquote");
 
-  let activeElement = 0;
+  let activeElement = 2;
 
   btnRight.addEventListener("click", function () {
     slides[activeElement].classList.add("visually-hidden");
@@ -73,9 +73,6 @@
   // переключатель блока частые вопросы
 
   let questions = document.querySelector(".questions__wrapper ul");
-  let questionsDescriptions = document.querySelector(
-    ".questions__wrapper ul li p"
-  );
 
   function openDescription(evt) {
     evt.preventDefault();
@@ -94,4 +91,72 @@
   }
 
   questions.addEventListener("click", openDescription);
+
+  // слайдер блока с фото
+
+  //   let dots = document.getElementsByClassName('slider__toggle'),
+  //     dotsArea = document.getElementById('slider'),
+  //     slidesItems = document.getElementsByClassName('card__item'),
+  //     slideIndex = 1;
+
+  //   showSlides(slideIndex);
+
+  //   function showSlides(n) {
+  //     if (n < 1) {
+  //       slideIndex = slidesItems.length;
+  //     } else if (n > slidesItems.length) {
+  //       slideIndex = 1;
+  //     }
+  //     for (let i = 0; i < slidesItems.length; i++) {
+  //       slidesItems[i].style.display = 'none';
+  //     }
+  //     for (let i = 0; i < dots.length; i++) {
+  //       dots[i].classList.remove('slider__toggle--active');
+  //     }
+  //     slidesItems[slideIndex - 1].style.display = 'block';
+  //     dots[slideIndex - 1].classList.add('slider__toggle--active');
+  //   }
+
+  //   function currentSlide(n) {
+  //     showSlides(slideIndex = n)
+  //   }
+
+  //   dotsArea.onclick = function (e) {
+  //     for (let i = 0; i < dots.length + 1; i++) {
+  //       if (e.target.classList.contains('slider__toggle') && e.target == dots[i - 1]) {
+  //         currentSlide(i);
+  //       }
+  //     }
+  //   }
+
+
+  const dotsArea = document.getElementById('card-slider');
+  const cardList = document.querySelector('.card__second-list').children;
+  let currentSlide = 2;
+
+
+  function toggleSlider(evt) {
+    evt.preventDefault();
+
+
+    if (evt.target.tagName != 'BUTTON') {
+      return;
+    }
+
+    const sliderId = +evt.target.dataset.sliderButton;
+
+    if (currentSlide == sliderId) {
+      return;
+    }
+    cardList[currentSlide - 2].classList.add('display-slider-none');
+    cardList[sliderId - 2].classList.remove('display-slider-none');
+
+    console.log(currentSlide)
+    console.log(sliderId)
+
+
+    currentSlide = sliderId;
+  }
+
+  dotsArea.addEventListener('click', toggleSlider);
 })();
